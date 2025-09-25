9777ac36c57169275d6749beb0ed1717bc06a717
Abdi <abdistar16@gmail.com>
2025-09-25 15:50:15 +0300

    cleanup

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -58,43 +58,7 @@ class CartController extends Controller
     }
 
 
-    // old code that are below this are that I am keeping for reference would be deleted later
-    protected function getCartIdentifier()
-    {
-        // Use user ID if authenticated, session ID if guest
-        return auth()->check() ? auth()->id() : session()->getId();
-    }
 
-
-    
-
-    protected function cartkey($identifier): string
-    {
-        $prefix = auth()->check() ? "cart:user:" : "cart:session:";
-        return $prefix . $identifier;
-    }
-
-    protected function getCartData($identifier)
-    {
-        $cart = Redis::hgetall($this->cartKey($identifier));
-
-        if (empty($cart)) {
-            return [];
-        }
-
-        $products = Product::whereIn('id', array_keys($cart))->get();
-
-        return $products->map(function ($product) use ($cart) {
-            $qty = (int) $cart[$product->id];
-            return [
-                'id' => $product->id,
-                'name' => $product->name,
-                'price' => $product->price,
-                'qty' => $qty,
-                'subtotal' => $product->price * $qty,
-            ];
-        });
-    }
 
    
 }
